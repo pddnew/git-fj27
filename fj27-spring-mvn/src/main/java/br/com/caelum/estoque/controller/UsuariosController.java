83eb46162c82588cfa24a4c19fece1161f267dce
danilo <danilo@danilo-laptop>
2013-08-11 23:34:20 -0300

    working on security settings

--- fj27-spring-mvn/src/main/java/br/com/caelum/estoque/controller/UsuariosController.java
+++ fj27-spring-mvn/src/main/java/br/com/caelum/estoque/controller/UsuariosController.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/")
 public class UsuariosController {
 
 	@RequestMapping(value="/login")
 	public String index(){
+		return "usuarios/login-form";
+	}
+	
+	@RequestMapping(value="/")
+	public String context(){
 		return "usuarios/login-form";
 	}
 }
